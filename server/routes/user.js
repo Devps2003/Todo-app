d36d280050811f621d788d8c1566dc7e8bbb53e5
Devps2003 <devpratap2003iit@gmail.com>
2024-03-29 02:26:47 +0530

    signup,signin api

--- server/routes/user.js
+++ server/routes/user.js
@@ -28,7 +28,49 @@ router.post("/signup", async(req,res)=>{
     const newUser = await User.create({
         username: req.body.username,
         email: req.body.email,
+        password: req.body.password,
+        todos: []
+    })
+    const newUserID= newUser._id;
+    const newTodo = new Todo({
+        userId: newUserID,
+        title: "My first Todo!",
+        description: "I will mark this todo done!",
+        completed: false
+    })
+    await newTodo.save();
+    newUser.todos.push(newTodo._id);
+    await newUser.save();
+    const token = jwt.sign({newUserID},jwt_secret.JWT_SECRET);
+    return res.status(201).json({ message: 'User created successfully' });
+
+})
+
+const signinBody = zod.object({
+    email: zod.string().email(),
+    password: zod.string()
+})
+router.post("/signin",async (req,res)=>{
+    const {success} = signinBody.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message: "wrong inputs"
+        })
+    }
+    const theUser = await User.findOne({
+        email: req.body.email,
         password: req.body.password
     })
-    
+    if(theUser){
+        const token = jwt.sign({userId:theUser._id},jwt_secret.JWT_SECRET);
+        return res.status(200).json({
+            message: "login successful"
+        })
+    }
+    return res.status(411).json({
+        message: "user not found"
+    })
 })
+
+
+router.post()
