91b387605db40a2daaf46381a551f6a9e16c0c92
Devps2003 <devpratap2003iit@gmail.com>
2024-03-30 01:13:53 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors')
-const rootRouter = require('./routes/index')
+const authRouter = require('./routes/auth');
+const todoRouter = require('./routes/todo');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api",rootRouter);
+app.use("/api/v1",authRouter);
+app.use("/api/v2",todoRouter);
 
 app.listen(3000,()=>{
     console.log("server runnnig")
---
+++ server/routes/auth.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const zod = require('zod');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+const router = express.Router();
+
+const signupBody = zod.object({
+    email: zod.string().email(),
+    username: zod.string(),
+    password: zod.string()
+});
+router.post("/signup",async(req,res)=>{
+    const {succes} = signupBody.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message: "wrong inputs"
+        })
+    }
+    try{
+        const hashpassword = bcrypt.hashSync(req.body.password);
+        const user = new User({
+            email: req.body.email,
+            username: req.body.username,
+            password: hashpassword
+        })
+        await user.save();
+        return res.status(200).json({
+            message: "user created"
+        })
+    }catch(error){
+        res.status(400).json({
+            message: "user already exist"
+        })
+    }
+})
+
+
+router.post("/signin", async(req,res)=>{
+    try{
+        const user = await User.findOne({
+            email: req.body.email
+        })
+        if(!user){
+            return res.status(411).json({
+                message: "user not found"
+            })
+        }
+        const ispasscorrect = bcrypt.compareSync(req.body.password, user.password);
+        if(!ispasscorrect){
+            return res.status(411).json({
+                message: "wrong password"
+            })
+        }
+        const {password, ...others}= user._doc;
+        res.status(200).json({user: others});
+
+    }catch(error){
+        res.status(411).json({
+            message: " internal server error"
+        })
+    }
+})
+
+module.exports=router;
